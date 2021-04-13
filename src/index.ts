83d7ea0bbb3d1895dacbb2ea481c5dee60875479
zhoujin <1391020381@qq.com>
2021-04-13 22:45:25 +0800

    headers

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { buildURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/headers'
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 function axios(config: AxiosRequestConfig): void {
@@ -7,9 +9,18 @@ function axios(config: AxiosRequestConfig): void {
 }
 function processConfig(config:AxiosRequestConfig):void{
   config.url = transformUrl(config)
+  config.data = transformRequest(config)
+  config.headers = transformHeaders(config)
 }
 function transformUrl(config:AxiosRequestConfig):string{
   const { url,params} = config
   return buildURL(url,params)
+}
+function transformRequestData(config:AxiosRequestConfig):any{
+  return transformRequest(config.data)
+}
+function transformHeaders(config:AxiosRequestConfig){
+  const { headers = {},data } = config
+  return processHeaders(headers,data)
 }
 export default axios
