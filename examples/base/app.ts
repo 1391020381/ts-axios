6460f24417f32f9d1a737d4b6d09d630510adcc1
zhoujin <171226Q2>
2021-04-15 10:10:24 +0800

    基本功能

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -1,112 +1,112 @@
 import axios from '../../src/index'
 
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: ['bar', 'baz']
-  }
-})
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: ['bar', 'baz']
+//   }
+// })
 //  /base/get?foo[]=bar&foo[]=baz'
 
 
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: {
-      bar: 'baz'
-    }
-  }
-})
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: {
+//       bar: 'baz'
+//     }
+//   }
+// })
 // /base/get?foo=%7B%22bar%22:%22baz%22%7D
 
 const date = new Date()
 
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    date
-  }
-})
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     date
+//   }
+// })
 
 // /base/get?date=2019-04-01T05:55:39.030Z
 
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: '@:$, '
-  }
-})
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: '@:$, '
+//   }
+// })
 // /base/get?foo=@:$+
 
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: 'bar',
-    baz: null
-  }
-})
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: 'bar',
+//     baz: null
+//   }
+// })
 
-axios({
-  method: 'get',
-  url: '/base/get#hash',
-  params: {
-    foo: 'bar'
-  }
-})
+// axios({
+//   method: 'get',
+//   url: '/base/get#hash',
+//   params: {
+//     foo: 'bar'
+//   }
+// })
 // /base/get?foo=bar
 
-axios({
-  method: 'get',
-  url: '/base/get?foo=bar',
-  params: {
-    bar: 'baz'
-  }
-})
+// axios({
+//   method: 'get',
+//   url: '/base/get?foo=bar',
+//   params: {
+//     bar: 'baz'
+//   }
+// })
 // /base/get?foo=bar&bar=baz
 
 
-axios({
-  method:'post',
-  url:'/base/post',
-  data:{
-    a:1,
-    b:2
-  }
-})
-const arr = new Int32Array([21,31])
+// axios({
+//   method:'post',
+//   url:'/base/post',
+//   data:{
+//     a:1,
+//     b:2
+//   }
+// })
+// const arr = new Int32Array([21,31])
 
-axios({
-  method:'post',
-  url:"/base/buffer",
-  data:arr
-})
+// axios({
+//   method:'post',
+//   url:"/base/buffer",
+//   data:arr
+// })
 
 
-axios({
-  method: 'post',
-  url: '/base/post',
-  headers: {
-    'content-type': 'application/json;'
-  },
-  data: {
-    a: 1,
-    b: 2
-  }
-})
+// axios({
+//   method: 'post',
+//   url: '/base/post',
+//   headers: {
+//     'content-type': 'application/json;'
+//   },
+//   data: {
+//     a: 1,
+//     b: 2
+//   }
+// })
 
-const paramsString = 'q=URLUtils.searchParams&topic=api'
-const searchParams = new URLSearchParams(paramsString)
+// const paramsString = 'q=URLUtils.searchParams&topic=api'
+// const searchParams = new URLSearchParams(paramsString)
 
-axios({
-  method: 'post',
-  url: '/base/post',
-  data: searchParams
-})
+// axios({
+//   method: 'post',
+//   url: '/base/post',
+//   data: searchParams
+// })
 
 axios({
   method: 'post',
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
 import { buildURL } from './helpers/url'
-import { transformRequest ,transformResponse} from './helpers/data'
+import { transformRequest, transformResponse } from './helpers/data'
 import { processHeaders } from './helpers/headers'
-import { AxiosRequestConfig,AxiosPromise ,AxiosResponse} from './types'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 import xhr from './xhr'
 function axios(config: AxiosRequestConfig): AxiosPromise {
   processConfig(config)
-  return xhr(config).then(res=>{
+  return xhr(config).then(res => {
     return transformResponseData(res)
   })
 }
-function processConfig(config:AxiosRequestConfig):void{
+function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
-  config.data = transformRequest(config)
   config.headers = transformHeaders(config)
+  config.data = transformRequestData(config)
 }
-function transformUrl(config:AxiosRequestConfig):string{
-  const { url,params} = config
-  return buildURL(url,params)
+function transformUrl(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
 }
-function transformRequestData(config:AxiosRequestConfig):any{
+function transformRequestData(config: AxiosRequestConfig): any {
   return transformRequest(config.data)
 }
-function transformHeaders(config:AxiosRequestConfig){
-  const { headers = {},data } = config
-  return processHeaders(headers,data)
+function transformHeaders(config: AxiosRequestConfig) {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
 }
 function transformResponseData(res: AxiosResponse): AxiosResponse {
   res.data = transformResponse(res.data)
