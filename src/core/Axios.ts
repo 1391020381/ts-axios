96a6a6320328839b6929c0ca7ede03088fbcace6
zhoujin <171226Q2>
2021-04-15 16:04:46 +0800

    extend

---
+++ src/core/Axios.ts
@@ -0,0 +1,45 @@
+import { AxiosRequestConfig, AxiosPromise, Method } from '../types'
+import dispathRequest from './dispatchRequest'
+export default class Axios {
+  request(config: AxiosRequestConfig): AxiosPromise {
+    return dispathRequest(config)
+  }
+  get(url: string, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithouData('get', url, config)
+  }
+  delete(url: string, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithData('delete', url, config)
+  }
+  head(url: string, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithouData('head', url, config)
+  }
+  options(url: string, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithouData('options', url, config)
+  }
+  post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithData('post', url, data, config)
+  }
+  put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithData('put', url, data, config)
+  }
+  patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithData('patch', url, data, config)
+  }
+  _requestMethodWithouData(method: Method, url: string, config?: AxiosRequestConfig) {
+    return this.request(
+      Object.assign(config || {}, {
+        method,
+        url
+      })
+    )
+  }
+  _requestMethodWithData(method: Method, url: string, data?: any, config?: AxiosRequestConfig) {
+    return this.request(
+      Object.assign(config || {}, {
+        method,
+        url,
+        data
+      })
+    )
+  }
+}
